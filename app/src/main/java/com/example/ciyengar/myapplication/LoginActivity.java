6a738bbe42a031f007d7290d6e90b0f47c5aff67
Crishna Iyengar <ciyengar3@gmail.com>
2016-02-08 23:25:44 -0500

    Changed Main

--- app/src/main/java/com/example/ciyengar/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/ciyengar/myapplication/LoginActivity.java
@@ -4,6 +4,7 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -331,8 +332,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
-
+                //startActivity(new Intent(MainActivity, MainActivity.class))
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();

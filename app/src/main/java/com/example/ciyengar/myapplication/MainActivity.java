e3bdc076e325ca48bc6caf149227e24d664f0a7a
Crishna Iyengar <ciyengar3@gmail.com>
2016-02-17 01:26:22 -0500

    Fixed Database Error; Everything should work for demo

--- app/src/main/java/com/example/ciyengar/myapplication/MainActivity.java
+++ app/src/main/java/com/example/ciyengar/myapplication/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ciyengar.myapplication;
 
 import android.os.Bundle;
+import java.util.Arrays;
 import android.support.design.widget.FloatingActionButton;
+import java.util.ArrayList;
+import android.util.Log;
 import android.widget.Button;
+import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,9 +15,49 @@ import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.NumberPicker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] majors = {"PICK A MAJOR",
+            "Architecture",
+            "Industrial Design",
+            "Computational Media",
+            "Computer Science",
+            "Aerospace Engineering",
+            "Biomedical Engineering",
+            "Chemical and Biomolecular Engineering",
+            "Civil Engineering",
+            "Computer Engineering",
+            "Electrical Engineering",
+            "Environmental Engineering",
+            "Industrial Engineering",
+            "Materials Science and Engineering",
+            "Mechanical Engineering",
+            "Nuclear and Radiological Engineering",
+            "Applied Mathematics",
+            "Applied Physics",
+            "Biochemistry",
+            "Biology",
+            "Chemistry",
+            "Discrete Mathematics",
+            "Earth and Atmospheric Sciences",
+            "Physics",
+            "Psychology",
+            "Applied Languages and Intercultural Studies",
+            "Computational Media",
+            "Economics",
+            "Economics and International Affairs",
+            "Global Economics and Modern Languages",
+            "History, Technology, and Society",
+            "International Affairs",
+            "International Affairs and Modern Language",
+            "Literature, Media, and Communication",
+            "Public Policy",
+            "Business Administration"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +65,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        EditText name = (EditText) findViewById(R.id.name);
+        TextView username = (TextView) findViewById(R.id.username);
+        NumberPicker major = (NumberPicker) findViewById(R.id.major);
+        major.setMinValue(0);// restricted number to minimum value i.e 1
+        major.setMaxValue(majors.length - 1);// restricked number to maximum value i.e. 31
+        //majorPicker.setWrapSelectorWheel(true);
+        major.setDisplayedValues(majors);
+        EditText password = (EditText) findViewById(R.id.password);
+        Button submit = (Button) findViewById(R.id.submit);
+        Button edit = (Button) findViewById(R.id.edit);
+        Button logout = (Button) findViewById(R.id.logout);
+        int numIndex = 0;
+        int counter = 0;
+        for (String s: majors) {
+            if (s.equals(LoginActivity.CURRENTLOGIN[3])) {
+                numIndex = counter;
+                break;
+            }
+            counter++;
+        }
+        edit.setEnabled(true);
+        logout.setEnabled(true);
+        submit.setEnabled(false);
+        name.setEnabled(false);
+        username.setEnabled(true);
+        major.setEnabled(false);
+        password.setEnabled(false);
+        name.setText(LoginActivity.CURRENTLOGIN[2]);
+        username.setText(LoginActivity.CURRENTLOGIN[0]);
+        major.setValue(numIndex);
+        password.setText(LoginActivity.CURRENTLOGIN[1]);
 
-        TextView showData = (TextView) findViewById(R.id.database);
-        showData.setText(niceMessage());
     }
 
     public void selfDestruct(View view) {
@@ -31,16 +104,73 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    /** Called when the user clicks the logout button */
-    public void logoutAshay(View view) {
-        // Do something in response to button
-        finish();
+    public void editProfile(View view) {
+        EditText name = (EditText) findViewById(R.id.name);
+        Button logout = (Button) findViewById(R.id.logout);
+        NumberPicker major = (NumberPicker) findViewById(R.id.major);
+        EditText password = (EditText) findViewById(R.id.password);
+        Button submit = (Button) findViewById(R.id.submit);
+        Button edit = (Button) findViewById(R.id.edit);
+        logout.setEnabled(false);
+        edit.setEnabled(false);
+        name.setEnabled(true);
+        major.setEnabled(true);
+        password.setEnabled(true);
+        submit.setEnabled(true);
+
     }
 
-    public String niceMessage() {
-        String output = "Hi " + LoginActivity.CURRENTLOGIN[2] + ". You are a great "
-            + LoginActivity.CURRENTLOGIN[3] + " major!";
-        return output;
+    public void submitChanges(View view) {
+        EditText password = (EditText) findViewById(R.id.password);
+        boolean cancel = false;
+        if (!isPasswordValid(password.getText().toString())) {
+            Context context = getApplicationContext();
+            CharSequence text = "Password is not valid";
+            int duration = Toast.LENGTH_SHORT;
+            cancel = true;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+        if (!cancel) {
+            updateDB();
+        }
+    }
+
+    private void updateDB() {
+        EditText name = (EditText) findViewById(R.id.name);
+        TextView username = (TextView) findViewById(R.id.username);
+        NumberPicker major = (NumberPicker) findViewById(R.id.major);
+        EditText password = (EditText) findViewById(R.id.password);
+        Button submit = (Button) findViewById(R.id.submit);
+        Button edit = (Button) findViewById(R.id.edit);
+        Button logout = (Button) findViewById(R.id.logout);
+        logout.setEnabled(true);
+        edit.setEnabled(true);
+        name.setEnabled(false);
+        username.setEnabled(false);
+        major.setEnabled(false);
+        password.setEnabled(false);
+        submit.setEnabled(false);
+        String nameText = name.getText().toString();
+        String usernameText = username.getText().toString();
+        String passwordText = password.getText().toString();
+        String majorText = majors[major.getValue()];
+        Log.d("Current Login", usernameText);
+        String index = LoginActivity.CURRENTLOGIN[4];
+        String changedUser = usernameText + ":" + passwordText + ":" + nameText + ":" + majorText + ":" + index;
+        LoginActivity.CURRENTLOGIN[0] = usernameText;
+        LoginActivity.CURRENTLOGIN[1] = passwordText;
+        LoginActivity.CURRENTLOGIN[2] = nameText;
+        LoginActivity.CURRENTLOGIN[3] = majorText;
+        int newIndex = Integer.parseInt(index);
+        LoginActivity.DATABASE.remove(index);
+        LoginActivity.DATABASE.add(newIndex, changedUser);
+    }
+
+    private boolean isPasswordValid(String password) {
+        if (password.length() < 5 || password.contains(":"))
+            return false;
+        return true;
     }
 
 }
